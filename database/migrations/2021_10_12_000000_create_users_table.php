5ab48677022d31851c9f373ceef32806dc10e267
Muhammad Faza <m.faza1998@gmail.com>
2021-02-16 22:34:20 +0700

    membuat migration roles & otp_codes beserta modelnya; tambah erd db;

--- database/migrations/2021_10_12_000000_create_users_table.php
+++ database/migrations/2021_10_12_000000_create_users_table.php
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('user_id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->uuid('role_id');
+            $table->uuid('otp_code_id');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('role_id')->references('role_id')->on('roles');
+            $table->foreign('otp_code_id')->references('otp_code_id')->on('otp_codes');
         });
     }
 
