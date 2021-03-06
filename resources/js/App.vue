616ab8cbd8d7132444d8a429b2644d96db67a8c9
Muhammad Faza <m.faza1998@gmail.com>
2021-03-06 19:03:16 +0700

    install vue & vue-router

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        <router-link to="/">Home</router-link>
+        <router-link to="/donations">Donations</router-link>
+
+        <div class="content-wrapper">
+            <router-view></router-view>
+        </div>
+        <!-- <app-footer/> -->
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('Component mounted.');
+    }
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,18 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/{any?}', 'app')->where('any', '.*');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Route::middleware(['auth', 'emailVerified'])->group(function ()
-{
-    Route::get('/route-1', 'HomeController@email');
-    Route::get('/route-2', 'HomeController@admin')->middleware('isAdmin');
-});
+// Route::middleware(['auth', 'emailVerified'])->group(function ()
+// {
+//     Route::get('/route-1', 'HomeController@email');
+//     Route::get('/route-2', 'HomeController@admin')->middleware('isAdmin');
+// });
