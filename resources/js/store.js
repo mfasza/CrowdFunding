c9f334b8dc5b1838292f25be5bcf03f0ccb8f6fb
Muhammad Faza <m.faza1998@gmail.com>
2021-03-11 12:15:18 +0700

    vuex persist

--- resources/js/store.js
+++ resources/js/store.js
@@ -4,10 +4,17 @@ import transaction from './store/transaction.js';
 import alert from './store/alert.js'
 import auth from './store/auth.js'
 import dialog from "./store/dialog.js";
+import VuexPersist from 'vuex-persist'
+
+const vuexPersist = new VuexPersist({
+  key: 'crowdfunding',
+  storage: localStorage
+})
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   modules: {
     transaction,
     alert,
